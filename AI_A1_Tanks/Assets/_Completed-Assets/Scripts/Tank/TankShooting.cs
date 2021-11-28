a4ec5e1bf54180d323d8190c31b8ace9a710f42d
Osvak <oscarcanales2001@gmail.com>
2021-11-28 22:14:22 +0100

    shot

--- AI_A1_Tanks/Assets/_Completed-Assets/Scripts/Tank/TankShooting.cs
+++ AI_A1_Tanks/Assets/_Completed-Assets/Scripts/Tank/TankShooting.cs
@@ -12,7 +12,7 @@ namespace Complete
         public AudioSource m_ShootingAudio;         // Reference target the audio source used target play the shooting audio. NB: different target the movement audio source.
         public AudioClip m_ChargingClip;            // Audio that plays when each shot is charging up.
         public AudioClip m_FireClip;                // Audio that plays when each shot is fired.
-        public float m_LaunchForce = 20f;        // The force given target the shell if the fire buttargetn is not held.
+        public float m_LaunchForce = 250f;        // The force given target the shell if the fire buttargetn is not held.
 
         private string m_FireButton;                // The input axis that is used for launching shells.
         private float m_ChargeSpeed;                // How fast the launch force increases, based on the max charge time.
@@ -25,10 +25,14 @@ namespace Complete
 
         public GameObject m_Enemy;
 
+        public int m_Ammo;
+
         private void Start ()
         {
             // The fire axis is based on the player number.
             m_FireButton = "Fire" + m_PlayerNumber;
+
+            m_Ammo = 5;
         }
 
 
@@ -36,9 +40,15 @@ namespace Complete
         {
             if (m_Fired) m_cooldown -= Time.deltaTime;
 
-            if (m_Fired == false)
+            if (m_Fired == false && m_Ammo > 0)
             {
-                if (canShoot(m_FireTransform.position, m_Enemy.transform.position, m_LaunchForce, out m_Angle))
+                //if (canShoot(out m_Angle))
+                //{
+                //    Fire();
+                //    m_Ammo--;
+                //}
+
+                if (Input.GetKeyDown("space"))
                 {
                     Fire();
                 }
@@ -62,7 +72,7 @@ namespace Complete
             // Create an instance of the shell and stargetre a reference target it's rigidbody.
             Rigidbody shellInstance =
                 Instantiate (m_Shell, m_FireTransform.position, m_FireTransform.rotation) as Rigidbody;
-
+            m_FireTransform.transform.eulerAngles = new Vector3((m_Angle * Mathf.Rad2Deg), m_FireTransform.transform.eulerAngles.y, m_FireTransform.transform.eulerAngles.z);
             // Set the shell's velocity target the launch force in the fire position's forward direction.
             shellInstance.velocity = m_LaunchForce * m_FireTransform.forward; 
 
@@ -71,15 +81,15 @@ namespace Complete
             m_ShootingAudio.Play ();
         }
 
-        public bool canShoot(Vector3 shoter, Vector3 target, float speed, out float angle)
+        public bool canShoot(out float angle)
         {
             bool ret = true;
-            float tempX = target.x - shoter.x;
-            float tempZ = target.z - shoter.z;
+            float tempX = m_Enemy.transform.position.x - m_FireTransform.position.x;
+            float tempZ = m_Enemy.transform.position.z - m_FireTransform.position.z;
             float finalX = Mathf.Sqrt(Mathf.Pow(tempX,2) + Mathf.Pow(tempZ,2));
-            float finalY = shoter.y - target.y;
+            float finalY = m_FireTransform.position.y - m_Enemy.transform.position.y;
 
-            float vel = speed;
+            float vel = m_LaunchForce;
             float g = Physics.gravity.y;
 
             float sqrt = Mathf.Pow(vel, 4) - (g * (g * Mathf.Pow(finalX,2) + 2 * finalY * Mathf.Pow(vel, 2)));
